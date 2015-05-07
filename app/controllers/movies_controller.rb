9cae3c05434c61d07e97c2785e867e9b7c210dd0
aleyvamcmurtry <aleyvamcmurtry@mills.edu>
2015-05-06 21:34:58 -0700

    Commit with comments

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,20 +5,23 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-#hw2 part 2
-#
+  #hw2 part 2
+  #if the user sets new rating parameters, sort into a hash and place 
+  #into an array
+  #if there already exists rating parameters, leave them
+  #if there is no rating parameters, show all movies with all ratings
   def index
     @all_ratings = Movie.all_ratings
     if params[:ratings]
-	@ratings_hash = params[:ratings]
-	@ratings_array = params[:ratings].keys
-	session[:ratings] = @ratings_hash
+	    @ratings_hash = params[:ratings]
+	    @ratings_array = params[:ratings].keys
+	    session[:ratings] = @ratings_hash
     elsif session[:ratings] 
-	flash.keep
-	redirect_to params.merge(:ratings => session[:ratings])
+	    flash.keep
+	    redirect_to params.merge(:ratings => session[:ratings])
     else
-	@ratings_hash = {}
-	@ratings_array = @all_ratings
+	    @ratings_hash = {}
+	    @ratings_array = @all_ratings
     end 
     #hw2 part 1.b 
     #if someone clicks on title, sorts and highlights
@@ -31,12 +34,11 @@ class MoviesController < ApplicationController
 		@release_date_header_class = 'hilite'
 	end
     elsif session[:sort_param] && params[:ratings]
-	flash.keep
-	redirect_to params.merge(:sort_param => session[:sort_param])
+	    flash.keep
+      redirect_to params.merge(:sort_param => session[:sort_param])
     end
 
-
-    @movies = Movie.find_all_by_rating(@ratings_array, :order => session[:sort_param])
+   @movies = Movie.find_all_by_rating(@ratings_array, :order => session[:sort_param])
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ActiveRecord::Base
   attr_accessible :title, :rating, :description, :release_date
 
-
+  #hw 2 part 2
+  #method for sorting ratings 
   def Movie.all_ratings
   	Movie.all.map{|movie| movie.rating}.uniq
   end
